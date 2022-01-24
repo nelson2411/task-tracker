9b88a9b71cd84f830cc2d07e9563d08ea22499b4
Nelson Rosales <56582884+nelson2411@users.noreply.github.com>
2022-01-24 08:31:17 +0100

    Project finished

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,11 @@
 <template>
-  <h1>About</h1>
+  <h3>Version 1.0.0</h3>
+  <router-link to="/">Go back home</router-link>
 </template>
+
+<script>
+export default {
+  name: "About",
+  inheritAttrs: false, // disable 'non-props' warning
+};
+</script>
